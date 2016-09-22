56d44e7ee0a9ff7090a00193887dde64df40ff3a
neven <neven@nevendemacbook-pro.local>
2016-09-23 07:25:45 +0800

    Initial Commit

---
+++ cptest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cptest
+//
+//  Created by neven on 16/9/23.
+//  Copyright © 2016年 neven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
